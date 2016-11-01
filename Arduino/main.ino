b41d3ca230c82f606aa5abc72296a760a0c55c7e
Nathanaël <nathanael.spriet@gmail.com>
2016-11-01 01:31:55 +0100

    Establishes the serial communication

--- Arduino/main.ino
+++ Arduino/main.ino
@@ -1,7 +1,25 @@
 // the setup function runs once when you press reset or power the board
-void setup() {
+void setup()
+{
+	// Open serial communications and wait for port to open:
+	Serial.begin(115200);
+	while (!Serial)
+	{
+		; // wait for serial port to connect. Needed for native USB port only
+	}
+
+	// print the welcome message
+	Serial.println("--- Welcome to the Arguiduino serial interface ---");
+
 }
 
 // the loop function runs over and over again forever
-void loop() {
+void loop()
+{
+	String stringFromSerial = Serial.readString();
+	if(stringFromSerial.length() > 0)
+	{
+		Serial.println("Received : "+stringFromSerial);
+	}
 }
+
